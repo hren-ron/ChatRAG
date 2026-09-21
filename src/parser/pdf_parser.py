86e9b0ea06712192a602b471c1fe9e01cf246490
hren <hren_ron@hotmail.com>
2026-09-22 05:50:44 +0800

    页眉、页脚检测去除

--- src/parser/pdf_parser.py
+++ src/parser/pdf_parser.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from typing import List, Dict, Tuple
 
 import fitz
+from sympy.physics.quantum.gate import normalized
+from typing_extensions import Counter
 
 
 class PDFParser:
@@ -65,7 +67,45 @@ class PDFParser:
         # -----------------------------------------------------
         # Step 2：检测页眉 / 页脚
         # -----------------------------------------------------
-        # headers, footers = self.detect_headers_footers(raw_pages)
+        headers, footers = self.detect_headers_footers(raw_pages)
+
+        # -----------------------------------------------------
+        # Step 3：清洗页面
+        # -----------------------------------------------------
+        cleaned_pages = []
+        for page in raw_pages:
+
+            lines = page["lines"]
+
+            lines = self.remove_header_footer(lines, headers, footers)
+
+            lines = self.merge_broken_lines(lines)
+
+            lines = [self.normalize_line(line) for line in lines]
+
+            lines = [line for line in lines if line]
+
+            text = "\n".join(lines)
+            if not text:
+                continue
+
+            cleaned_pages.append(
+                {
+                    "page": page["page"],
+                    "text": text
+                }
+            )
+
+        # -----------------------------------------------------
+        # Step 4：识别章节结构
+        # -----------------------------------------------------
+        print(cleaned_pages)
+        structured_pages = self.detect_structure(cleaned_pages)
+        print(structured_pages)
+        return structured_pages
+
+
+
 
     # =========================================================
     # 2. PDF 提取
@@ -271,10 +311,250 @@ class PDFParser:
     # 6. 页眉 / 页脚检测
     # =========================================================
     def detect_headers_footers(self, pages:List[Dict]) -> Tuple[set, set]:
-        pass
+
+        page_count = len(pages)
+
+        if page_count < 3:
+            return set(), set()
+
+        header_counter = Counter()
+        footer_counter = Counter()
+
+        for page in pages:
+            lines = page["lines"]
+
+            # 页面前N行
+            header_part = lines[:self.header_lines]
+
+            # 页面后N行
+            footer_part = lines[-self.footer_lines:]
+
+            for line in header_part:
+                normalized = self.normalize_line(line)
+
+                if normalized:
+                    header_counter[normalized] += 1
+
+            for line in footer_part:
+                normalized = self.normalize_line(line)
+
+                if normalized:
+                    footer_counter[normalized] += 1
+
+        threshold_count = max(2, int(page_count * self.threshold))
+
+        headers = {
+            text for text,count in header_counter.items() if count > threshold_count
+        }
+
+        footers = {
+            text for text, count in footer_counter.items() if count > threshold_count
+        }
+
+        return headers, footers
+
+    # =========================================================
+    # 7. 删除页眉 / 页脚 / 页码
+    # =========================================================
+    def remove_header_footer(
+        self,
+        lines: List[str],
+        headers: set,
+        footers: set
+    ) -> List[str]:
+
+        result = []
+
+        for line in lines:
+            normalized = self.normalize_line(line)
+
+            # 页码
+            if self.is_page_number(normalized):
+                continue
+
+            if normalized in headers:
+                continue
+
+            if normalized in footers:
+                continue
+
+            result.append(line)
+        return result
+
+    # =========================================================
+    # 8. 页码判断
+    # =========================================================
+    @staticmethod
+    def is_page_number(line: str) -> bool:
+
+        line = line.strip()
+
+        patterns = [
+            # 1
+            r"^\d+$",
+            # - 1 -
+            r"^-\s*\d+\s*-$",
+            # — 1 —
+            r"^—\s*\d+\s*—$",
+            # 第 1 页
+            r"^第\s*\d+\s*页$",
+            # 1 / 100
+            r"^\d+\s*/\s*\d+$",
+        ]
+
+        return any(re.match(pattern, line) for pattern in patterns)
+
+    # =========================================================
+    # 9. 跨行文本合并
+    # =========================================================
+    @staticmethod
+    def merge_broken_lines(lines: List[str]) -> List[str]:
+
+        result = []
+
+        i = 0
+
+        while i < len(lines):
+            current = lines[i].strip()
+
+            # -------------------------------------------------
+            # 3. + 1 → 3.1
+            # -------------------------------------------------
+            if (
+                re.fullmatch(r"\d+\.", current)
+                and i + 1 < len(lines)
+                and re.fullmatch(r"\d+", lines[i+1].strip())
+            ):
+
+                merged = (current + lines[i+1].strip())
+
+                result.append(merged)
+
+                i += 2
+
+                continue
+
+            # -------------------------------------------------
+            # GB/T3730. + 1
+            # -------------------------------------------------
+            if (
+                re.search(r"(GB/T|GB)\s*\d+\.$", current, re.IGNORECASE)
+                and i + 1 < len(lines)
+                and re.fullmatch(r"\d+", lines[i+1].strip())
+            ):
+                merged = (current + lines[i+1].strip())
+
+                result.append(merged)
+
+                i += 1
+
+                continue
+
+            result.append(current)
+
+            i += 1
+        return result
+
+    # =========================================================
+    # 11. 章节识别
+    # =========================================================
+    def detect_structure(self, pages: List[Dict]) -> List[Dict]:
+
+        current_chapter = None
+        current_section = None
+
+        result = []
+
+        for page in pages:
+
+            page_number = page["page"]
+
+            lines = page["text"].splitlines()
+
+            content_lines = []
+            for line in lines:
+
+                line = line.strip()
+                if not line:
+                    continue
+
+                # -------------------------------------------------
+                # 一级章节
+                #
+                # 1 范围
+                # 2 规范性引用文件
+                # 3 术语和定义
+                # -------------------------------------------------
+
+                chapter_match = re.match(r"^(\d+)\s+(.+)$", line)
+
+                if chapter_match:
+                    number = chapter_match.group(1)
+                    title = chapter_match.group(2)
+
+                    current_chapter = (f"{number} {title}")
+                    current_section =  None
+                    continue
+
+                # -------------------------------------------------
+                # 二级章节
+                #
+                # 3.1 车辆长度
+                # 3.2 车辆宽度
+                # -------------------------------------------------
+                section_match = re.match(r"^(\d+\.\d+)\s*(.*)$", line)
+
+                if section_match:
+                    number = section_match.group(1)
+                    title = section_match.group(2).strip()
+
+                    # 如果标题被拆到下一行
+                    if not title:
+                        current_section = number
+                    else:
+                        current_section = (f"{number} {title}")
+
+                    continue
+
+                # -------------------------------------------------
+                # 如果上一行只有 3.1
+                #
+                # 下一行是：
+                #
+                # 车辆长度 vehicle length
+                # -------------------------------------------------
+
+                if (
+                    current_section
+                    and re.fullmatch(r"\d+\.\d+", current_section)
+                ):
+                    current_section = (f"{current_section} {line}")
+                    continue
+
+                content_lines.append(line)
+
+            # -----------------------------------------------------
+            # 处理当前页面正文
+            # -----------------------------------------------------
+
+            text = "\n".join(content_lines)
+
+            if not text:
+                continue
+
+            result.append(
+                {
+                    "page": page_number,
+                    "chapter": current_chapter,
+                    "section": current_section,
+                    "text": text
+                }
+            )
+
+        return result
 
     @staticmethod
-    def clean_text(text: str) -> str:
+    def normalize_line(text: str) -> str:
 
         # 去掉首位空白
         text = text.strip()
@@ -285,28 +565,20 @@ class PDFParser:
         # 连续空格压缩
         text = re.sub(r"[ \t]+", " ", text)
 
-        # 连续空行压缩
-        text = re.sub(r"\n{3,}", "\n\n", text)
-
         # 标准编号处理
+        # -----------------------------------------------------
+        # GB/T3730.1
+        #
+        # →
+        #
+        # GB/T 3730.1
+        # -----------------------------------------------------
         text = re.sub(
             r"GB/T\s*(\d+).\s*(\d+)",
-            r"GB/T\1.\2",
+            r"GB/T \1.\2",
             text
         )
 
-        # 中英文之间补空格
-        text = re.sub(
-            r"([\u4e00-\u9fff])([A-Za-z])",
-            r"\1 \2",
-            text
-        )
-
-        text = re.sub(
-            r"([A-Za-z]([\u4e00-\u9fff]))",
-            r"\1 \2",
-            text
-        )
 
         return text
 
